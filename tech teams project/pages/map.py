aa021dcfddeca959023b9612077cc48cb1828d25
Jadams87 <142312489+Jadams87@users.noreply.github.com>
2023-08-15 13:01:56 -0230

    Update map.py
    fixed the map so it works correctly and does not lag

--- tech teams project/pages/map.py
+++ tech teams project/pages/map.py
@@ -1,9 +1,10 @@
 import streamlit as st
+from streamlit_folium import st_folium
+from streamlit_extras.colored_header import colored_header
 import streamlit.components.v1 as components
+import time
 import folium
 from folium import plugins
-from streamlit_extras.colored_header import colored_header
-import time
 
 colored_header(
     label="Map",
@@ -11,35 +12,24 @@ colored_header(
     color_name="violet-70",
 )
 
-#st.info("This is a map for the west coast of NewfoundLand that has")
-
 st.sidebar.success("Please select a page.")
 
+left_col, right_col = st.columns([150,50])
 
-m = folium.Map(location = [49.1852,-57.4184])
-folium.Marker([49.1852, -57.4184], popup='<i>Deer Lake</i>', tooltip='click me').add_to(m)
-#folium.Marker([48.9523, -57.9460], popup='<i>Corner Brook</i>', tooltip='click me').add_to(m)
-
-#folium.Circle(location=[48.9523, -57.9460], radius=200, popup='<i>Corner Brook</i>',tooltip='click me').add_to(m)
-
-plugins.Draw().add_to(m)
-
-m.save('map.html')
-
-col1, col2, col3, col4 = st.columns(4)
 
 if st.button('Click to view map'):
     with st.spinner(text='In progress'):
-        with col1:
+        with left_col:
             time.sleep(3)
-            HtmlFile = open("map.html", 'r', encoding='utf-8')
-            source_code = HtmlFile.read() 
-            print(source_code)
-            components.html(source_code, height = 600, width = 536)
-            time.sleep(1)
-        with col4:
+            map_test = folium.Map(location = [49.1852,-57.4184])
+            folium.Marker([49.1852, -57.4184], popup='<i>Deer Lake</i>', tooltip='click me').add_to(map_test)
+            #folium.Marker([48.9523, -57.9460], popup='<i>Corner Brook</i>', tooltip='click me').add_to(map_test)
+            #folium.Circle(location=[48.9523, -57.9460], radius=200, popup='<i>Corner Brook</i>',tooltip='click me').add_to(m)
+            plugins.Draw().add_to(map_test)
+            st_map = st_folium(map_test, width = 850, height = 600, returned_objects=['last_onject_clicked'])
+        with right_col:
             with st.expander("See Legend"):
                 HtmlFile = open("legend.html", 'r', encoding='utf-8')
                 source_code = HtmlFile.read() 
                 print(source_code)
-                components.html(source_code, height = 600)
+                components.html(source_code, height = 765)
